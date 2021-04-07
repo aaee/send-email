a8dbe723a4c9bbd48c901ab170d2260f0f5be1e1
Alicia Esquivias Roman <a.esquivias-roman@klarna.com>
2021-04-07 11:19:58 +0200

    added german idioms part

--- send_email.py
+++ send_email.py
@@ -24,24 +24,29 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
 sender = environ['SENDER']
 receiver = environ['RECEIVER']
 receiver_cc = environ['RECEIVER_CC']
+receiver_german = environ['RECEIVER_GERMAN']
+receiver_german_cc = environ['RECEIVER_GERMAN_CC']
 refresh_token_var = environ['REFRESH_TOKEN']
 token_uri = environ['TOKEN_URI']
 client_id = environ['CLIENT_ID']
 client_secret = environ['CLIENT_SECRET']
+    
+
 
 dir_abs = path.dirname(__file__)
 dir_abs = dir_abs + '/' if len(dir_abs) > 0 else dir_abs
 
 
 def main():
-    schedule.every().day.at("09:30").do(send_email)
+    #schedule.every().day.at("09:30").do(send_email)
     #schedule.every(5).minutes.do(send_email)
+    send_email()
 
        
-    while True:
+    #while True:
     # run_pending
-      schedule.run_pending()
-      time.sleep(1)
+    #  schedule.run_pending()
+    #  time.sleep(1)
  
 
 
@@ -65,7 +70,7 @@ def send_email():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         elif refresh_token_var:
-          print('trying this new thing')
+          print('using refresh tocket for OAUTH')
           creds = credentials.Credentials(None,refresh_token=refresh_token_var, token_uri=token_uri, client_id=client_id, client_secret=client_secret)
           creds.refresh(Request())
         else:
@@ -73,22 +78,28 @@ def send_email():
             flow = InstalledAppFlow.from_client_secrets_file(
                 dir_abs + 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=8080)
-            #creds = service_account.Credentials.from_service_account_file(dir_abs + 'credentials.json', scopes=SCOPES)
-            #delegated_credentials = creds.with_subject('ali.abril4@gmail.com')
+
         # Save the credentials for the next run
         with open(dir_abs + 'token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('gmail', 'v1', credentials=creds)
-    # send email
+    # send email - ice breaker
     subject = 'Good morning!'
     message_text = create_content_ice_breaker()
-    print('sending')
-    print(message_text)
+    print('sending icebreaker email')
+    #print(message_text)
     message_1 = create_message(sender, receiver, subject, message_text, cc=receiver_cc)
-    #message_2 = create_message(sender, 'nikhil.ninne@gmail.com', subject, message_text, cc='alicia.esquivias@gmail.com, nikhil.ninne@gmail.com')
     send_message(service, 'me', message_1)
-    #send_message(service, 'me', message_2)
+    
+    # send email - german idiom
+    message_text_german = create_content_german()
+    print('sending german email')
+    #print(message_text_german)
+    subject_german = "Kein Problem!!"
+    message_2 = create_message(sender, receiver_german, subject_german, message_text_german, cc=receiver_cc, parse_html=True)
+    send_message(service, 'me', message_2)
+
 
 
 
@@ -175,13 +186,62 @@ Best,
         lines_joke = [i for i in range(1,1622)]  
         selection_joke = choice(lines_joke)    
         daily_joke = jokes_file.readlines()[selection_joke]
-        print(daily_joke)
         daily_joke = daily_joke.split(',', maxsplit=1)
       
     return text.format(joke=daily_joke[1], quote=daily_quote[0], by=daily_quote[1], topic=daily_quote[2])
 
 
-def create_message(sender, to, subject, message_text, cc=None):
+def create_content_german():
+    font_family = "'Open Sans','Helvetica Neue',Helvetica,Arial,sans-serif"
+    text = '''
+    <div style="margin-left:10%; margin-right:10%">
+    <h4 style="font-family:{font_family};font-size:20px;font-style:normal;font-weight:bold;line-height:150%;letter-spacing:1px;text-align:center">Hallo Blanchis Burger!</h4>
+
+    <p style="font-family:{font_family};font-size:18px;font-style:normal;font-weight:normal;line-height:100%;letter-spacing:1px;text-align:center">Ich m&ouml;chte dass wir perfekt Deutsch sprechen k&ouml;nnen. Daf&uuml;r m&uuml;ssen wir viele Redewendungen lernen.<span>&nbsp;</span></p>
+
+    <p style="color:#43404d;font-family:{font_family};font-size:16px;font-style:normal;font-weight:normal;line-height:100%;letter-spacing:2px;text-align:center"><span>UNTEN FINDEST DU DIE HEUTIGEN REDEWENDUNGEN:</span></p>
+    
+    <hr style="text-align:center;color:gray;border: 0 none;border-top:dotted 1px;">
+
+    <p style="color:#4485b8;font-family:{font_family};font-size:20px;font-style:normal;font-weight:bold;line-height:100%;text-align:center"><span>{idiom}</span></p>
+    
+    <hr style="text-align:center;color:gray;border: 0 none;border-top:dotted 1px;">
+
+    <p style="color:#43404d;font-family:{font_family};font-size:16px;font-style:normal;font-weight:bold;line-height:100%;letter-spacing:1px;text-align:center"><span>W&ouml;rtliche &Uuml;bersetzung:</span></p>
+    <p style="color:#43404d;font-family:{font_family};font-size:16px;font-style:normal;font-weight:normal;line-height:100%;letter-spacing:1px;text-align:center"><em>{literal_translation}</em></p>
+    <p style="color:#43404d;font-family:{font_family};font-size:16px;font-style:normal;font-weight:bold;line-height:100%;letter-spacing:1px;text-align:center"><span>Englische &Uuml;bersetzung:</span></p>
+    <p style="color:#43404d;font-family:{font_family};font-size:16px;font-style:normal;font-weight:normal;line-height:100%;letter-spacing:1px;text-align:center"><em>{english_translation}</em></p>
+    <p style="color:#43404d;font-family:{font_family};font-size:16px;font-style:normal;font-weight:bold;line-height:100%;letter-spacing:1px;text-align:center"><span><b class="b5">Spanische</b> &Uuml;bersetzung:</span></p>
+    <p style="color:#43404d;font-family:{font_family};font-size:16px;font-style:normal;font-weight:normal;line-height:100%;letter-spacing:1px;text-align:center"><em>{spanish_translation}</em></p>
+    
+    <hr style="text-align:center;color:gray;border: 0 none;border-top:dotted 1px;width:80%"">
+    <p style="text-align: center;"><img src="{image}" alt= "" width="70%" height="auto""img" />
+    
+    <hr style="text-align:center;color:gray;border: 0 none;border-top:dotted 1px;"">
+    
+    <p style="font-family:{font_family};font-size:18px;font-style:normal;font-weight:normal;line-height:100%;letter-spacing:1px;text-align:center">Ich w&uuml;nsche dir einen sch&ouml;nen Tag!</p>
+    <p style="font-family:{font_family};font-size:18px;font-style:normal;font-weight:normal;line-height:100%;letter-spacing:1px;text-align:center">Ali's german bot :)&nbsp;</p>
+    <p></p>
+
+    </div>
+    '''
+    # get index for idiom
+    first_day = datetime.strptime('2021-04-07', '%Y-%m-%d')
+    days_diff = (datetime.now() - first_day).days + 1
+    #print(days_diff)
+
+    with open(dir_abs + 'german_idioms.csv', 'r') as german_idioms:
+        daily_idiom= german_idioms.readlines()[days_diff]
+        daily_idiom = daily_idiom.split(';')
+
+
+
+    return text.format(font_family=font_family, idiom=daily_idiom[0], literal_translation=daily_idiom[1], english_translation=daily_idiom[2], spanish_translation=daily_idiom[3], image=daily_idiom[4])
+
+
+
+
+def create_message(sender, to, subject, message_text, cc=None, parse_html=False):
   """Create a message for an email.
 
   Args:
@@ -193,13 +253,16 @@ def create_message(sender, to, subject, message_text, cc=None):
   Returns:
     An object containing a base64url encoded email object.
   """
-  message = MIMEText(message_text)
+  if parse_html is True:
+    message = MIMEText(message_text, 'html')
+  else:
+    message = MIMEText(message_text)
+  
   message['to'] = to
   message['from'] = sender
   message['subject'] = subject
   if cc is not None:
       message['cc'] = cc
-  # print(base64.urlsafe_b64encode(message.as_string().encode()).decode())
   return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
 
 
